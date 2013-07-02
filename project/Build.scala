a604e30b7e799b09bea38bee7f7a155608e28c59
nico <nico@3k-rouge.home>
2013-07-02 23:34:37 +0200

    Add: dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -10,9 +10,10 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "org.mongodb" %% "casbah" % "2.6.2",
+    "joda-time" % "joda-time" % "2.2"
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
