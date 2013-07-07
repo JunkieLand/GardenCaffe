c97cdc1ee902d75c7e70a74529cbcbacf7acf063
nico <nico@3k-rouge.home>
2013-07-07 13:58:51 +0200

    Fix: ajax form submission.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -9,59 +9,61 @@ var initDatePickers = function() {
 
 var initBookingForm = function() {
   var form = $("#bookingForm"),
-      onSubmit = function(e) {
-        e.preventDefault();
-        var hasError = false;
+  form.on("submit", function(e) {
+    e.preventDefault();
+    var hasError = false;
 
-        $(".control-group").removeClass("error");
-        $(".help-block").hide();
+    $(".control-group").removeClass("error");
+    $(".help-block").hide();
 
-        var nameCtrl = $("#nameCtrl");
-        if(nameCtrl.find("input").val().length == 0){
-          hasError = true;
-          nameCtrl.addClass("error");
-          nameCtrl.find(".help-block").show();
-        }
+    var nameCtrl = $("#nameCtrl");
+    if(nameCtrl.find("input").val().length == 0){
+      hasError = true;
+      nameCtrl.addClass("error");
+      nameCtrl.find(".help-block").show();
+    }
 
-        var emailCtrl = $("#emailCtrl");
-        var emailVal = emailCtrl.find("input").val();
-        if(emailVal.length < 3 || emailVal.indexOf("@") == -1){
-          hasError = true;
-          emailCtrl.addClass("error");
-          emailCtrl.find(".help-block").show();
-        }
+    var emailCtrl = $("#emailCtrl");
+    var emailVal = emailCtrl.find("input").val();
+    if(emailVal.length < 3 || emailVal.indexOf("@") == -1){
+      hasError = true;
+      emailCtrl.addClass("error");
+      emailCtrl.find(".help-block").show();
+    }
 
-        var peopleNbCtrl = $("#peopleNbCtrl");
-        var peopleNbVal = peopleNbCtrl.find("input").val();
-        if(peopleNbVal.length == 0 || !peopleNbVal.match(/^\d*$/) || parseInt(peopleNbVal) == 0) {
-          hasError = true;
-          peopleNbCtrl.addClass("error");
-          peopleNbCtrl.find(".help-block").show();
-        }
+    var peopleNbCtrl = $("#peopleNbCtrl");
+    var peopleNbVal = peopleNbCtrl.find("input").val();
+    if(peopleNbVal.length == 0 || !peopleNbVal.match(/^\d*$/) || parseInt(peopleNbVal) == 0) {
+      hasError = true;
+      peopleNbCtrl.addClass("error");
+      peopleNbCtrl.find(".help-block").show();
+    }
 
-        var fromCtrl = $("#fromCtrl"),
-          fromVal = $("#fromInput").val(),
-          from = moment(fromVal, "DD-MM-YYYY"),
-          toCtrl = $("#toCtrl"),
-          toVal = $("#toInput").val(),
-          to = moment(toVal, "DD-MM-YYYY");
-        if(fromVal.length == 0 || toVal.length == 0 ||
-           from.isBefore(moment().subtract("days", 1)) || to.isBefore(from) ||
-          to.diff(from, "days") == 0) {
-          hasError = true;
-          fromCtrl.addClass("error");
-          fromCtrl.find(".help-block").show();
-          toCtrl.addClass("error");
-        }
+    var fromCtrl = $("#fromCtrl"),
+      fromVal = $("#fromInput").val(),
+      from = moment(fromVal, "DD-MM-YYYY"),
+      toCtrl = $("#toCtrl"),
+      toVal = $("#toInput").val(),
+      to = moment(toVal, "DD-MM-YYYY");
+    if(fromVal.length == 0 || toVal.length == 0 ||
+      from.isBefore(moment().subtract("days", 1)) || to.isBefore(from) ||
+      to.diff(from, "days") == 0) {
+      hasError = true;
+      fromCtrl.addClass("error");
+      fromCtrl.find(".help-block").show();
+      toCtrl.addClass("error");
+    }
 
-        if(!hasError) {
-          $("#bookingSubmit").hide();
-          $("#bookingSuccess").show();
-          form.off("submit", onSubmit);
-          form.submit();
-        }
-      };
-  form.on("submit", onSubmit);
+    if(!hasError) {
+      $("#bookingSubmit").hide();
+      $("#bookingSuccess").show();
+      $.ajax({
+        url: form.attr("action"),
+        type: "POST",
+        data: form.serialize()
+      });
+    }
+  });
 
   $("#fromInput, #toInput").on("change", function() {
     var fromVal = $("#fromInput").val(),
@@ -78,43 +80,45 @@ var initBookingForm = function() {
 
 var initContactForm = function() {
   var form = $("#contactForm"),
-    onSubmit = function(e) {
-      e.preventDefault();
-      var hasError = false;
+  form.on("submit", function(e) {
+    e.preventDefault();
+    var hasError = false;
 
-      $(".control-group").removeClass("error");
-      $(".help-block").hide();
+    $(".control-group").removeClass("error");
+    $(".help-block").hide();
 
-      var nameCtrl = $("#nameCtrl");
-      if(nameCtrl.find("input").val().length == 0){
-        hasError = true;
-        nameCtrl.addClass("error");
-        nameCtrl.find(".help-block").show();
-      }
+    var nameCtrl = $("#nameCtrl");
+    if(nameCtrl.find("input").val().length == 0){
+      hasError = true;
+      nameCtrl.addClass("error");
+      nameCtrl.find(".help-block").show();
+    }
 
-      var emailCtrl = $("#emailCtrl");
-      var emailVal = emailCtrl.find("input").val();
-      if(emailVal.length < 3 || emailVal.indexOf("@") == -1){
-        hasError = true;
-        emailCtrl.addClass("error");
-        emailCtrl.find(".help-block").show();
-      }
+    var emailCtrl = $("#emailCtrl");
+    var emailVal = emailCtrl.find("input").val();
+    if(emailVal.length < 3 || emailVal.indexOf("@") == -1){
+      hasError = true;
+      emailCtrl.addClass("error");
+      emailCtrl.find(".help-block").show();
+    }
 
-      var msgCtrl = $("#msgCtrl");
-      if(msgCtrl.find("textarea").val().length == 0){
-        hasError = true;
-        msgCtrl.addClass("error");
-        msgCtrl.find(".help-block").show();
-      }
+    var msgCtrl = $("#msgCtrl");
+    if(msgCtrl.find("textarea").val().length == 0){
+      hasError = true;
+      msgCtrl.addClass("error");
+      msgCtrl.find(".help-block").show();
+    }
 
-      if(!hasError) {
-        $("#contactSubmit").hide();
-        $("#contactSuccess").show();
-        form.off("submit", onSubmit);
-        form.submit();
-      }
-    };
-  form.on("submit", onSubmit);
+    if(!hasError) {
+      $("#contactSubmit").hide();
+      $("#contactSuccess").show();
+      $.ajax({
+        url: form.attr("action"),
+        type: "POST",
+        data: form.serialize()
+      });
+    }
+  });
 };
 
 
